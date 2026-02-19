c6edb0ff21ea07c0bff05346bc8c6827562da611
santaes <oleksandrsukharyev@gmail.com>
2026-02-19 15:31:30 +0100

    add icon settings tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,7 +34,7 @@ export default function TabLayout() {
         name="settings"
         options={{
           title: 'Settings',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="gearshape.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="gear" color={color} />,
         }}
       />
     </Tabs>
